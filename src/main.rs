bb8ee1f3a1e66edf7d7df9b13c09a5ea7fb4c907
Bálits Péter <balitspm@gmail.com>
2025-05-12 17:02:03 +0200

    A fresh start

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,47 @@ use std::env;
 #[allow(unused_imports)]
 use std::fs;
 
-fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    eprintln!("Logs from your program will appear here!");
+use clap::{arg, Parser, Subcommand};
 
-    // Uncomment this block to pass the first stage
-    // let args: Vec<String> = env::args().collect();
-    // if args[1] == "init" {
-    //     fs::create_dir(".git").unwrap();
-    //     fs::create_dir(".git/objects").unwrap();
-    //     fs::create_dir(".git/refs").unwrap();
-    //     fs::write(".git/HEAD", "ref: refs/heads/main\n").unwrap();
-    //     println!("Initialized git directory")
-    // } else {
-    //     println!("unknown command: {}", args[1])
-    // }
+#[derive(Parser, Debug)]
+#[command(version,about,long_about=None)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Init,
+    #[command()]
+    CatFile {
+        #[arg(short = 'p')]
+        pretty_print: bool,
+        #[arg()]
+        hash: String,
+    },
+}
+
+fn main() -> anyhow::Result<()> {
+    let args = Args::parse();
+
+    match args.cmd {
+        Commands::Init => {
+            let args: Vec<String> = env::args().collect();
+            if args[1] == "init" {
+                fs::create_dir(".git")?;
+                fs::create_dir(".git/objects")?;
+                fs::create_dir(".git/refs")?;
+                fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
+                println!("Initialized git directory")
+            } else {
+                println!("unknown command: {}", args[1])
+            }
+        }
+        Commands::CatFile { pretty_print, hash } => {
+            todo!()
+        }
+    }
+
+    Ok(())
 }
