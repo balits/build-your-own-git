c5436b1521a4da4700bd76141af31e03a9ea333b
Bálits Péter <balitspm@gmail.com>
2025-05-12 17:08:34 +0200

    add(cat-file)

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,12 @@
 use std::env;
 #[allow(unused_imports)]
 use std::fs;
+use std::io;
+use std::io::BufRead;
+use std::io::Read;
+use std::io::Write;
 
+use anyhow::Context;
 use clap::{arg, Parser, Subcommand};
 
 #[derive(Parser, Debug)]
@@ -41,7 +46,47 @@ fn main() -> anyhow::Result<()> {
             }
         }
         Commands::CatFile { pretty_print, hash } => {
-            todo!()
+            anyhow::ensure!(pretty_print, "Only pretty printing is supported now");
+            anyhow::ensure!(hash.len() == 40, "Only supporting 40 length hashes");
+
+            let path = format!(".git/objects/{}/{}", &hash[..2], &hash[2..]);
+            let file = fs::File::open(path).context("failed to open file")?;
+            let mut reader = io::BufReader::new(file);
+            let mut buf = Vec::new();
+
+            reader
+                .read_until(0, &mut buf)
+                .context("Failed to read header")?;
+            let header = std::str::from_utf8(&buf).context("Parsing header")?;
+            let (kind, size) = header.strip_suffix("\0").unwrap().split_once(" ").unwrap();
+            let size = size
+                .parse::<usize>()
+                .context("Couldnt parse header's size")?;
+
+            if kind != "blob" {
+                anyhow::bail!("Only reading blobs is supported");
+            }
+
+            let mut z = flate2::bufread::ZlibDecoder::new(reader);
+
+            buf.clear();
+            buf.resize(size, 0);
+            z.read_to_end(&mut buf)
+                .context(format!("Reading {} bytes from object", size))?;
+            let more = z
+                .read(&mut buf)
+                .context("Attempting to read more from object")?;
+            anyhow::ensure!(
+                more == 0,
+                format!(
+                    "Object contained {} more bytes than expected {}",
+                    more, size
+                )
+            );
+            io::stdout()
+                .lock()
+                .write_all(&buf)
+                .context("Couldnt write to stdout")?;
         }
     }
 
