a7e788d063f602001ae9ded3ae38b37e0eb59d91
Bálits Péter <balitspm@gmail.com>
2025-05-17 00:00:35 +0200

    feat: chunked reading of files + then compression

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,3 @@
-#[allow(unused_imports)]
-use std::env;
-use std::ffi::CString;
-#[allow(unused_imports)]
 use std::fs;
 use std::io;
 use std::io::BufRead;
@@ -9,10 +5,12 @@ use std::io::Read;
 use std::io::Write;
 
 use anyhow::Context;
-use bytes::Buf;
-use bytes::BufMut;
 use clap::{arg, Parser, Subcommand};
-use flate2::FlushCompress;
+use flate2::Compression;
+use sha1::digest::Update;
+use sha1::{Digest, Sha1};
+
+const READ_CHUNK_SIZE: usize = 1024 * 1;
 
 #[derive(Parser, Debug)]
 #[command(version,about,long_about=None)]
@@ -23,6 +21,7 @@ struct Args {
 
 #[derive(Subcommand, Debug)]
 enum Commands {
+    #[command()]
     Init,
     #[command()]
     CatFile {
@@ -31,23 +30,27 @@ enum Commands {
         #[arg()]
         hash: String,
     },
+    #[command()]
+    HashObject {
+        #[arg(short)]
+        write: bool,
+
+        #[arg()]
+        filename: String,
+    },
 }
 
 fn main() -> anyhow::Result<()> {
+    env_logger::init();
     let args = Args::parse();
 
     match args.cmd {
         Commands::Init => {
-            let args: Vec<String> = env::args().collect();
-            if args[1] == "init" {
-                fs::create_dir(".git")?;
-                fs::create_dir(".git/objects")?;
-                fs::create_dir(".git/refs")?;
-                fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
-                println!("Initialized git directory")
-            } else {
-                println!("unknown command: {}", args[1])
-            }
+            fs::create_dir(".git")?;
+            fs::create_dir(".git/objects")?;
+            fs::create_dir(".git/refs")?;
+            fs::write(".git/HEAD", "ref: refs/heads/main\n")?;
+            println!("Initialized git directory")
         }
         Commands::CatFile { pretty_print, hash } => {
             anyhow::ensure!(pretty_print, "Only pretty printing is supported now");
@@ -78,6 +81,64 @@ fn main() -> anyhow::Result<()> {
                 .lock()
                 .write_all(&buf)
                 .context("Couldnt write to stdout")?;
+        }
+        Commands::HashObject { write, filename } => {
+            let mut total = 0;
+            let mut reader = io::BufReader::new(fs::File::open(&filename).context("Opening file")?);
+            let mut buf = vec![0u8; READ_CHUNK_SIZE];
+            let mut hasher = Sha1::new();
+
+            let attr = fs::metadata(&filename)
+                .context(format!("Reading metadata of file {}", &filename))?;
+            let filesize = attr.len(); // < TOUTOC or smth
+            if !attr.is_file() {
+                anyhow::bail!("Only hashing blobs is supported");
+            }
+
+            if write {
+                let outf = fs::File::create("tmp").context("Creating temporary file")?;
+                let w = io::BufWriter::new(outf);
+                let mut z = flate2::write::ZlibEncoder::new(w, Compression::default());
+                let mut i = 0;
+                loop {
+                    let n = reader
+                        .read(&mut buf[..])
+                        .context("Reading chunks of the file")?;
+                    if n == 0 {
+                        //EOF
+                        break;
+                    }
+                    total += n;
+                    log::debug!("Chunk({}):  {} bytes of {}", i, total, attr.len());
+                    Update::update(&mut hasher, &buf[..n]);
+                    z.write(&buf[..n]).context("Writing chunk to the file")?;
+                    i += 1;
+                }
+                z.flush().context("Flushing remaining bytes to the file")?;
+
+                log::debug!("File size {}, got {}", &filesize, &total);
+            } else {
+                loop {
+                    let n = reader
+                        .read(&mut buf[..])
+                        .context("Reading chunks of the file")?;
+                    if n == 0 {
+                        //EOF
+                        break;
+                    }
+                    total += n;
+                    Update::update(&mut hasher, &buf[..n]);
+                }
+            }
+
+            anyhow::ensure!(
+                total as u64 == filesize,
+                "Expected file size {filesize} got {total}"
+            );
+            Update::update(&mut hasher, "blob \0".as_bytes());
+            Update::update(&mut hasher, format!("{}", filesize).as_bytes());
+
+            println!("{:x}", hasher.finalize());
         }
     }
 
